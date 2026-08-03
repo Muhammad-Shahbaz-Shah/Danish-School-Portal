468419577e92806833ed26ef88c6bd6ac3bbba09
User <user@example.com>
2026-08-02 22:53:11 -0700

    Error Fixed Deployment

--- api/db.js
+++
@@ -1,4 +0,0 @@
-import serverless from 'serverless-http';
-import serverModule from '../dist/server.cjs';
-const app = serverModule.default || serverModule;
-export default serverless(app);
--- api/db.ts
+++
@@ -1,5 +0,0 @@
-import server from '../server';
-import serverless from 'serverless-http';
-
-// Export a Serverless Function that Vercel will expose at /api/db/*
-export default serverless(server);
